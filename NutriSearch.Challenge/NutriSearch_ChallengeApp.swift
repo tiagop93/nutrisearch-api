7d6370e79671d66faf1ad347a367cd7d63dd0448
Tiago Pereira <tiago_pereira_93@hotmail.com>
2025-04-02 13:17:56 +0100

    Initial Commit

---
+++ NutriSearch.Challenge/NutriSearch_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriSearch_ChallengeApp.swift
+//  NutriSearch.Challenge
+//
+//  Created by Tiago Pereira on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct NutriSearch_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
